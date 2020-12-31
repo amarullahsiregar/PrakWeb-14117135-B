fa687a85c5aaa02b62e3a8276fd7546008e169ee
Rahman Amarullah - Windows <45584180+amarullahsiregar@users.noreply.github.com>
2021-01-01 02:17:03 +0700

    Update tugas-1.php

--- minggu-3/tugas-1.php
+++ minggu-3/tugas-1.php
@@ -1,13 +1,45 @@
-<?php
-function faktorial($value){
+<!DOCTYPE html>
+<html lang="en">
 
-    if($value<=1){
-        return 1;
-    }else{
-        return $value * faktorial($value - 1);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mencari Faktorial</title>
+</head>
+
+<body>
+
+    <fieldset>
+        <h2 style="font-size:30pt;">Mencari Bilangan Faktorial</h2>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <p>
+                <label>Masukkan Bilangan :</label><br>
+                <input type="text" id="textbox" name="num" style="font-size:12pt; height:30px; width:800px;" />
+            </p>
+            <p>
+                <input type="submit" name="submit" style="background-color:#2a77e8; color:white; height:40px; width:100px;" value="Cari Faktorial" />
+            </p>
+
+        </form>
+    </fieldset>
+
+    <?php
+    function faktorial($value)
+    {
+
+        if ($value <= 1) {
+            return 1;
+        } else {
+            return $value * faktorial($value - 1);
+        }
     }
-}
 
-    echo "nilai faktorial dari 4 = ".faktorial(4);
+    if (isset($_POST['submit'])) {
+        $bilangan = $_POST['num'];
+        faktorial($bilangan);
+    }
 
-?>
+    ?>
+</body>
+
+</html>
